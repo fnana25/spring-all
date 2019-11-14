88fd973aa88d436fac8d534b9f16487b56a638f7
fengna <692098869@qq.com>
2019-11-14 10:27:19 +0800

    添加注解

--- chapter4/redis-demo/src/main/java/com/example/redisdemo/RedisDemoApplication.java
+++ chapter4/redis-demo/src/main/java/com/example/redisdemo/RedisDemoApplication.java
@@ -1,9 +1,15 @@
 package com.example.redisdemo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@Slf4j
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableTransactionManagement
 public class RedisDemoApplication {
 
 	public static void main(String[] args) {
