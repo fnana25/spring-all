36039fe5b739c8fd67762b59379e337a1c3f0dee
fengna <692098869@qq.com>
2019-11-18 16:15:17 +0800

    cache-with-redis-demo

---
+++ chapter4/cache-with-redis-demo/src/main/java/com/example/cachewithredisdemo/CacheWithRedisDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cachewithredisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheWithRedisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheWithRedisDemoApplication.class, args);
+	}
+
+}
