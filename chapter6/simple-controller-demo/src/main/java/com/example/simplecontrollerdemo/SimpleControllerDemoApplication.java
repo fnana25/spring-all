b404e259c1cb4072bd1ba1a29fc20d3336318ada
fengna <692098869@qq.com>
2019-11-19 15:41:23 +0800

    simple-controller-demo

---
+++ chapter6/simple-controller-demo/src/main/java/com/example/simplecontrollerdemo/SimpleControllerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplecontrollerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleControllerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleControllerDemoApplication.class, args);
+	}
+
+}
