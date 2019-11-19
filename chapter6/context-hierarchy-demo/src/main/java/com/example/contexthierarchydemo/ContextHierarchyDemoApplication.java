dc27ad2ee5524750e7d087301e08d89439c0c3e3
fengna <692098869@qq.com>
2019-11-19 18:08:10 +0800

    context-hierarchy-demo

---
+++ chapter6/context-hierarchy-demo/src/main/java/com/example/contexthierarchydemo/ContextHierarchyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.contexthierarchydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContextHierarchyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContextHierarchyDemoApplication.class, args);
+	}
+
+}
