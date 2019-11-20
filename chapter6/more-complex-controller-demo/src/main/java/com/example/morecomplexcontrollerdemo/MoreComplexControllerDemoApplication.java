bf97285c43a819aa51885d9b8c7fce2221a437f3
fengna <692098869@qq.com>
2019-11-20 15:14:11 +0800

    more-complex-controller-demo

---
+++ chapter6/more-complex-controller-demo/src/main/java/com/example/morecomplexcontrollerdemo/MoreComplexControllerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.morecomplexcontrollerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoreComplexControllerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoreComplexControllerDemoApplication.class, args);
+	}
+
+}
---
+++ chapter6/more-complex-controller-demo/src/test/java/com/example/morecomplexcontrollerdemo/MoreComplexControllerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.morecomplexcontrollerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoreComplexControllerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
