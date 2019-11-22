1520b03bb7eb5ab5e760eeeeeab8788ec4a85d28
fengna <692098869@qq.com>
2019-11-22 10:24:11 +0800

    thymeleaf-view-demo

---
+++ chapter6/thymeleaf-view-demo/src/main/java/com/example/thymeleafviewdemo/ThymeleafViewDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafviewdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafViewDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafViewDemoApplication.class, args);
+	}
+
+}
