0cd4b01c61e35cef6f39e82b9923bf585cdb3e32
fengna <692098869@qq.com>
2019-12-23 10:27:11 +0800

    init

---
+++ chapter7/complex-resttemplate-demo/src/main/java/com/example/complexresttemplatedemo/ComplexResttemplateDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.complexresttemplatedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplexResttemplateDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplexResttemplateDemoApplication.class, args);
+	}
+
+}
