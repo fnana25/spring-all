0a70566eee8fc6a88356096dd5ed6a0e42a39cb1
fengna <692098869@qq.com>
2020-01-16 17:20:33 +0800

    nacos-waiter

---
+++ chapter12/nacos-waiter-service/src/main/java/com/example/nacoswaiterservice/model/OrderState.java
@@ -0,0 +1,5 @@
+package com.example.nacoswaiterservice.model;
+
+public enum OrderState {
+    INIT, PAID, BREWING, BREWED, TAKEN, CANCELLED
+}
---
+++ chapter12/nacos-waiter-service/src/main/java/com/example/nacoswaiterservice/repository/CoffeeRepository.java
@@ -0,0 +1,11 @@
+package com.example.nacoswaiterservice.repository;
+
+import com.example.nacoswaiterservice.model.Coffee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CoffeeRepository extends JpaRepository<Coffee, Long> {
+    List<Coffee> findByNameInOrderById(List<String> list);
+    Coffee findByName(String name);
+}
