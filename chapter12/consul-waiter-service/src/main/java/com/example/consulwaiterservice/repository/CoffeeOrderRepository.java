32844a18b1ae96de3f32239ba01b2a6490bd644a
fengna <692098869@qq.com>
2020-01-16 16:48:35 +0800

    consul-waiter

---
+++ chapter12/consul-waiter-service/src/main/java/com/example/consulwaiterservice/repository/CoffeeOrderRepository.java
@@ -0,0 +1,7 @@
+package com.example.consulwaiterservice.repository;
+
+import com.example.consulwaiterservice.model.CoffeeOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CoffeeOrderRepository extends JpaRepository<CoffeeOrder, Long> {
+}
